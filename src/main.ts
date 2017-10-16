d653319b67d0727a14f5598b87539c88057df30e
aelmod <aelmod@mail.ru>
2017-10-16 16:04:35 +0300

    added AngularJs bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { setUpLocationSync } from '@angular/router/upgrade';
+
+declare const angular: any;
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  const upgrade = ref.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.body, ['myApp'], { strictDi: true });
+  setUpLocationSync(upgrade);
+}).catch(console.error);
